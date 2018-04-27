f2f0f49f5c184fda0afd05b59bc9ee34edd6de67
Peter Shutka <shutka.intel@gmail.com>
2018-04-27 22:06:46 +0300

    Completed script for testing OpenAPI

--- Deployment/deployment.ps1
+++ Deployment/deployment.ps1
@@ -1,13 +1,37 @@
-Param(
-	[ValidateSet("local", "remove")]
+﻿Param(
+	[ValidateSet("LocalStorage", "RemoteStorage")]
+	[Parameter(Mandatory=$false)]
 	[string]
-	$storageType
+	$storageType = "LocalStorage",
+	[int]
+	[Parameter(Mandatory=$false)]
+	[ValidateRange(10000,100000)] 
+	$port = 60508
 )
 
 $sourceCodeRepository = "https://github.com/PeterIntel/UserService.git"
-$postmanCollection = ".\User.Service\User.Service\Postman\Users.postman_collection.json"
-$postmanEnvironment = ".\User.Service\User.Service\Postman\Dev.postman_environment.json"
-$dotnetWorkingDirectory = ".\Service.User\Service.User"
+$postmanCollection = ".\UserService\User.Service\User.Service\Postman\Users.postman_collection.json"
+$postmanEnvironment = ".\UserService\User.Service\User.Service\Postman\Dev.postman_environment.json"
+$dotnetWorkingDirectory = ".\UserService\User.Service\User.Service"
 $newmanReportFile = "newman_report.json"
 
-Write-Output $storageType
+$env:ASPNETCORE_URLS = "http://localhost:$port"
+git clone $sourceCodeRepository
+$dotnet = Start-Process "dotnet.exe" -ArgumentList "run --launch-profile $storageType --project $dotnetWorkingDirectory" -passthru
+Write-Output "PowerShell is waiting before the service will have been launched..."
+
+while($true){
+	
+	try{
+		Get-NetTCPConnection -LocalPort $port -ErrorAction Stop
+		newman run $postmanCollection -e $postmanEnvironment -r cli,json --reporter-json-export $newmanReportFile
+		Stop-Process -Name "dotnet"
+		Remove-Item -Path '.\UserService' -Recurse -Force
+		break
+	}
+	catch{
+	}
+}
+
+
+
