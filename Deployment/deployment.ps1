2da97eaa671c9e576b4dbd66316e6312910aaab5
Peter <shutka.intel@gmail.com>
2018-04-30 16:10:04 +0300

    Added possibility for saving history of log files

--- Deployment/deployment.ps1
+++ Deployment/deployment.ps1
@@ -13,7 +13,7 @@ $sourceCodeRepository = "https://github.com/PeterIntel/UserService.git"
 $postmanCollection = ".\UserService\User.Service\User.Service\Postman\Users.postman_collection.json"
 $postmanEnvironment = ".\UserService\User.Service\User.Service\Postman\Dev.postman_environment.json"
 $dotnetWorkingDirectory = ".\UserService\User.Service\User.Service"
-$newmanReportFile = "..\TestResults\newman_report.json"
+$newmanReportFile = "..\TestResults\newman_report"
 
 $env:ASPNETCORE_URLS = "http://localhost:$port"
 git clone $sourceCodeRepository
@@ -21,10 +21,10 @@ $dotnet = Start-Process "dotnet.exe" -ArgumentList "run --launch-profile $storag
 Write-Output "PowerShell is waiting before the service will have been launched..."
 
 while($true){
-	
 	try{
+		$newmanReportFile = "..\TestResults\newman_report_$(Get-Date -UFormat '%Y-%m-%d_%H-%M-%S')";
 		Get-NetTCPConnection -LocalPort $port -ErrorAction Stop
-		newman run $postmanCollection -e $postmanEnvironment -r cli,json --reporter-json-export $newmanReportFile
+		newman run $postmanCollection -e $postmanEnvironment -r cli,json,html --reporter-json-export "$($newmanReportFile)$('.json')" --reporter-html-export "$($newmanReportFile)$('.html')"
 		Stop-Process -Name "dotnet"
 		Remove-Item -Path '.\UserService' -Recurse -Force
 		break
